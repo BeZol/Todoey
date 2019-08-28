82d62b69c96ad46bd6a460865ca70b8ccfb7a54e
BeZol <bezol.hun@gmail.com>
2019-08-28 20:24:39 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Beke Zoltán on 2019. 08. 28..
+//  Copyright © 2019. Zoltán Beke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
