c75fc7d05144824cc1b4090ed5c5e1bb6bfffc4b
BeZol <bezol.hun@gmail.com>
2019-09-06 09:02:28 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Beke Zoltán on 2019. 09. 03..
+//  Copyright © 2019. Zoltán Beke. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
